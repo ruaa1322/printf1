2802893cec7fbc288ce088e407b86fec7e9b7ca9
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-10-14 13:03:31 -0700

    updated functions and printf

--- main.h
+++ main.h
@@ -8,24 +8,11 @@
 
 int put_char(char c);
 int put_str(char *string);
-/*<<<<<<< HEAD*/
-int _printf(const char *format, ...);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int _strlen(char *s);
-int _strlenc(const char *s);
-int _strlenc(const char *s);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
-int _putchar(char c);
-
 int pnt37(void);
 int pntch(va_list value);
 int pntstr(va_list value);
 int _printf(const char *format, ...);
 int str_len(char *str);
 int str_len_c(const char *str);
-/*>>>>>>> 84473613dbaa84a5e512141168b04b8a927be8c0*/
 
 #endif
