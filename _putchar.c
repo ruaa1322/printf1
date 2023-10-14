6e5e9e4646060ebc984a198f5495dc82b3c53017
ruaa1322 <ruaa000@hotmail.com>
2023-10-14 10:10:37 -0700

    ruaa

--- _putchar.c
+++ _putchar.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
 *_putchar - writes the charcter c to etdout
@@ -9,5 +9,5 @@
 */
 int _putchar(char c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
