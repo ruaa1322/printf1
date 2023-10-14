8ffa92be9969420a55957b992e592cc0b9221bb0
ruaa1322 <ruaa000@hotmail.com>
2023-10-14 10:26:24 -0700

    dd

--- _printf.c
+++ _printf.c
@@ -1,45 +1,51 @@
 #include "main.h"
+
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+ * _printf - printf function
+ * @format: character string
+ * @...: shows function is variadic
+ * Return: t_len
  */
-int _printf(const char * const format, ...)
+
+int _printf(const char *format, ...)
 {
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
+        int t_len = 0;
+        va_list value;
 
-	va_list args;
-	int i = 0, j, len = 0;
+        va_start(value, format);
+        while (*format != '\0')
+        {
+                if (*format == '%')
+                {
+                        format++;
+                        switch (*format)
+                        {
+                                case '%':
+                                        t_len += put_char('%');
+                                        break;
+                                case 'c':
+                                        t_len += put_char(va_arg(value, int));
+                                        break;
+                                case 's':
+                                {
+                                        char *string = va_arg(value, char *);
 
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-Here:
-	while (format[i] != '\0')
-	{
-		j = 13;
-		while (j >= 0)
-		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
-		}
-		_putchar(format[i]);
-		len++;
-		i++;
-	}
-	va_end(args);
-	return (len);
+                                        if (string == NULL)
+                                                t_len += put_str("(nil)");
+                                        else
+                                                t_len += put_str(string);
+                                        break;
+                                }
+                                default:
+                                        t_len += put_char('%');
+                                        t_len += put_char(*format);
+                                        break;
+                        }
+                }
+                else
+                        t_len += put_char(*format);
+                format++;
+        }
+        va_end(value);
+        return (t_len);
 }
