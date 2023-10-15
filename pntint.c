d245c0c6cf5149f29f4ad0d470f36d355e2ee110
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-10-15 01:44:22 -0700

    updated pntint

--- pntint.c
+++ pntint.c
@@ -11,7 +11,9 @@ int pntint(va_list value)
 	int n = va_arg(value, int); /* retrieve the integer from value */
 	int t_len = 0; /*initialize total length*/
 	int dg; /*used to get last value of n*/
+	int i;
 	char dg_char; /*used to conver int to char*/
+	char buf[12];
 
 	if (n < 0) /*if integer is negative*/
 	{
@@ -25,9 +27,13 @@ int pntint(va_list value)
 	{
 		dg = n % 10;
 		dg_char = '0' + dg;
-		put_char(dg_char);
+		buf[i] = dg_char;
 		n /= 10;
-		t_len++;
+		i++;
 	}
+
+	for (i--; i >= 0; i--) 
+		t_len += put_char(buf[i]);
+
 	return (t_len);
 }
---
+++ testcases/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(void)
+{
+	int a = 10;
+	char c = 'h';
+	char s[] = "hello";
+
+	_printf("%d, %c, %s\n", a, c, s);
+}
