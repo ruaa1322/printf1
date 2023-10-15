6e199a93cd56f3a2176fc9e18e52ecfee05dcae8
AbdullahArmiyao <abdulbnarmi@gmail.com>
2023-10-14 23:25:51 -0700

    checking if format is NULL inr _printf.c

--- _printf.c
+++ _printf.c
@@ -12,6 +12,8 @@ int _printf(const char *format, ...)
 	int t_len = 0; /*inintialize total length*/
 	va_list value; /*declare argument list*/
 
+	if (format == NULL)
+		return (-1);
 	va_start(value, format); /*initialize the arguments with format*/
 	while (*format != '\0') /*while we are still on a character*/
 	{
@@ -28,10 +30,8 @@ int _printf(const char *format, ...)
 					t_len += pntch(value);
 					break;
 				case 's':
-				{
 					t_len += pntstr(value);
 					break;
-				}
 				default:
 				t_len += put_char('%') + put_char(*format);
 				break;
