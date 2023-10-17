8f6d5b7f98486c3af1a3ea622998a55514e3adef
ruaa1322 <ruaa000@hotmail.com>
2023-10-17 10:49:41 -0700

    hh

--- main.h
+++ main.h
@@ -5,6 +5,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define UNUSED(x) (void)(x)
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+#define S_LONG 2
+#define S_SHORT 1
 
 #define BUF_SIZE 1024
 /**
